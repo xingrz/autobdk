f7afe560fe886d0f2a1cd9c3a0f558c92180634b
XiNGRZ <hi@xingrz.me>
2021-09-26 14:59:47 +0800

    Submit approval

--- src/api.ts
+++ src/api.ts
@@ -64,15 +64,21 @@ export async function getAttendanceRecordList(cookie: string, yearmo = ''): Prom
   return data;
 }
 
+export enum IAttendanceClockType {
+  上班 = 1,
+  下班 = 2,
+}
+
 export interface IAttendanceRecord {
   bdkErrorMessage: null;
   bdkStatus: number;
   isFinish: number;
   isShowClockTime: number;
   signTimeList: {
+    clockAttribution: IAttendanceClockType;
     clockTime: string;  // "12:45"
     rangeId: string;
-    rangeName: '上班' | '下班';
+    rangeName: keyof typeof IAttendanceClockType;
     statusDesc: string;  // 空字符串时表示没异常
   }[];
   situationDesc: null;
@@ -113,4 +119,63 @@ export async function getApproveBdkFlow(cookie: string, date: string): Promise<I
     },
   }).json() as IEnvelope<IApproveBdkFlow[]>;
   return data;
+}
+
+interface INewSignAgain {
+  departmentList: {
+    departmentId: string;
+    departmentName: string;
+  }[];
+  flowSettingId: number;
+  flow_type: number;  // 6
+  flow_type_desc: string;  // "补卡"
+}
+
+export async function newSignAgain(cookie: string): Promise<INewSignAgain> {
+  const { data } = await http.post(`${XRXS_URL}/attendance/ajax-new-sign-again`, {
+    headers: {
+      Cookie: cookie,
+    },
+  }).json() as IEnvelope<INewSignAgain>;
+  return data;
+}
+
+export interface IAttendanceApproval {
+  flow_type: number;
+  flowSettingId: number;
+  departmentId: string;
+  date: string;  // "1630425600"
+  start_date: string;  // "2021-09-01 10:00"
+  timeRangeId: string;
+  bdkDate: string;  // "2021-09-01"
+  clockType: IAttendanceClockType,
+}
+
+// data: {"flow_type":6,"flowSettingId":2880415,"departmentId":"5aeccaaec68a4dcc91029f1d84621319","isClocking":0,"date":"1630425600","start_date":"2021-09-01 10:00","reason":"","image_path":"","timeRangeId":"2027489","bdkDate":"2021-09-01","clockType":1,"rangeModels":[],"custom_field":"[]"}
+// data: {"flow_type":6,"flowSettingId":2880415,"departmentId":"5aeccaaec68a4dcc91029f1d84621319","isClocking":0,"date":"1630857600","start_date":"2021-09-06 19:00","reason":"","image_path":"","timeRangeId":"2027489","bdkDate":"2021-09-06","clockType":2,"rangeModels":[],"custom_field":"[]"}
+
+export async function startAttendanceApproval(cookie: string, approval: IAttendanceApproval): Promise<void> {
+  const data = JSON.stringify({
+    flow_type: approval.flow_type,
+    flowSettingId: approval.flowSettingId,
+    departmentId: approval.departmentId,
+    isClocking: 0,
+    date: approval.date,
+    start_date: approval.start_date,
+    reason: '',
+    image_path: '',
+    timeRangeId: approval.timeRangeId,
+    bdkDate: approval.bdkDate,
+    clockType: approval.clockType,
+    rangeModels: [],
+    custom_field: "[]"
+  });
+  await http.post(`${XRXS_URL}/attendance/ajax-start-attendance-approval`, {
+    headers: {
+      Cookie: cookie,
+    },
+    form: {
+      data,
+    },
+  }).json();
 }
